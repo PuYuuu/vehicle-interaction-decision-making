98269a0bcb30238ee8958ee6647b3b42ebac4d1a
yu.pu <yuu.pu@foxmail.com>
2024-02-13 12:30:55 +0800

    add safezone in Vehicle

--- scripts/vehicle.py
+++ scripts/vehicle.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 
 class Vehicle:
-    def __init__(self, x, y, yaw, length = 5, width = 2, color = 'k'):
+    def __init__(self, x, y, yaw, length = 5, width = 2,
+                 color = 'k', safe_length = 8, safe_width = 2.4):
         self.x = x
         self.y = y
         self.yaw = yaw
@@ -11,7 +12,10 @@ class Vehicle:
         self.length = length
         self.width = width
         self.color = color
+        self.safe_length = safe_length
+        self.safe_width = safe_width
         self.vehicle_box2d = self.get_box2d()
+        self.safezone = self.get_safezone()
 
 
     def get_box2d(self):
@@ -19,13 +23,27 @@ class Vehicle:
             [[-self.length/2, self.length/2, self.length/2, -self.length/2, -self.length/2],
             [self.width/2, self.width/2, -self.width/2, -self.width/2, self.width/2]]
         )
-        rot1 = np.array([[np.cos(self.yaw), -np.sin(self.yaw)],
+        rot = np.array([[np.cos(self.yaw), -np.sin(self.yaw)],
                      [np.sin(self.yaw), np.cos(self.yaw)]])
 
-        vehicle = np.dot(rot1, vehicle)
+        vehicle = np.dot(rot, vehicle)
         vehicle += np.array([[self.x], [self.y]])
 
         return vehicle
+
+
+    def get_safezone(self):
+        safezone = np.array(
+            [[-self.safe_length/2, self.safe_length/2, self.safe_length/2, -self.safe_length/2, -self.safe_length/2],
+            [self.safe_width/2, self.safe_width/2, -self.safe_width/2, -self.safe_width/2, self.safe_width/2]]
+        )
+        rot = np.array([[np.cos(self.yaw), -np.sin(self.yaw)],
+                     [np.sin(self.yaw), np.cos(self.yaw)]])
+
+        safezone = np.dot(rot, safezone)
+        safezone += np.array([[self.x], [self.y]])
+
+        return safezone
 
 
     def update(self, acc, omega, dt):
@@ -35,15 +53,16 @@ class Vehicle:
         self.yaw += omega * dt
 
         self.vehicle_box2d = self.get_box2d()
+        self.safezone = self.get_safezone()
 
 
     def draw_vehicle(self):
         head = np.array(
             [[0.3 * self.length, 0.3 * self.length], [self.width/2, -self.width/2]]
         )
-        rot1 = np.array([[np.cos(self.yaw), -np.sin(self.yaw)],
+        rot = np.array([[np.cos(self.yaw), -np.sin(self.yaw)],
                      [np.sin(self.yaw), np.cos(self.yaw)]])
-        head = np.dot(rot1, head)
+        head = np.dot(rot, head)
         head += np.array([[self.x], [self.y]])
 
         self.vehicle_box2d = self.get_box2d()
@@ -52,16 +71,16 @@ class Vehicle:
         plt.plot(head[0, :], head[1, :], self.color)
 
 
-    def has_overlap(self, box2d) -> bool:
-        box2d_sides_num = len(box2d[0])
+    @staticmethod
+    def has_overlap(box2d_0, box2d_1) -> bool:
         total_sides = []
-        for i in range(1, len(self.vehicle_box2d[0])):
-            vec_x = self.vehicle_box2d[0][i] - self.vehicle_box2d[0][i - 1]
-            vec_y = self.vehicle_box2d[1][i] - self.vehicle_box2d[1][i - 1]
+        for i in range(1, len(box2d_0[0])):
+            vec_x = box2d_0[0][i] - box2d_0[0][i - 1]
+            vec_y = box2d_0[1][i] - box2d_0[1][i - 1]
             total_sides.append([vec_x, vec_y])
-        for i in range(1, box2d_sides_num):
-            vec_x = box2d[0][i] - box2d[0][i - 1]
-            vec_y = box2d[1][i] - box2d[1][i - 1]
+        for i in range(1, len(box2d_1[0])):
+            vec_x = box2d_1[0][i] - box2d_1[0][i - 1]
+            vec_y = box2d_1[1][i] - box2d_1[1][i - 1]
             total_sides.append([vec_x, vec_y])
 
         for i in range(len(total_sides)):
@@ -69,23 +88,23 @@ class Vehicle:
 
             vehicle_min = np.inf
             vehicle_max = -np.inf
-            for j in range(1, len(self.vehicle_box2d[0])):
-                project = separating_axis[0] * self.vehicle_box2d[0][j] + separating_axis[1] * self.vehicle_box2d[1][j]
+            for j in range(0, len(box2d_0[0])):
+                project = separating_axis[0] * box2d_0[0][j] + separating_axis[1] * box2d_0[1][j]
                 vehicle_min = min(vehicle_min, project)
                 vehicle_max = max(vehicle_max, project)
-            
+
             box2d_min = np.inf
             box2d_max = -np.inf
-            for j in range(0, box2d_sides_num):
-                project = separating_axis[0] * box2d[0][j] + separating_axis[1] * box2d[1][j]
+            for j in range(0, len(box2d_1[0])):
+                project = separating_axis[0] * box2d_1[0][j] + separating_axis[1] * box2d_1[1][j]
                 box2d_min = min(box2d_min, project)
                 box2d_max = max(box2d_max, project)
 
             if vehicle_min > box2d_max or box2d_min > vehicle_max:
                 return False
-    
+
         return True
-    
+
 
 if __name__ == "__main__":
     print("has_overlap() test ...")
@@ -103,15 +122,15 @@ if __name__ == "__main__":
         [[5, -25], [5, -25]]
     ]
 
-    bool_has_overlap = vehicle.has_overlap(rect[0])
+    bool_has_overlap = Vehicle.has_overlap(vehicle.get_box2d(), rect[0])
     print("has_overlap with rect[0]: ", bool_has_overlap)
-    bool_has_overlap = vehicle.has_overlap(rect[1])
+    bool_has_overlap = Vehicle.has_overlap(vehicle.get_box2d(), rect[1])
     print("has_overlap with rect[1]: ", bool_has_overlap)
-    bool_has_overlap = vehicle.has_overlap(laneline[0])
+    bool_has_overlap = Vehicle.has_overlap(vehicle.get_box2d(), laneline[0])
     print("has_overlap with laneline[0]: ", bool_has_overlap)
-    bool_has_overlap = vehicle.has_overlap(laneline[1])
+    bool_has_overlap = Vehicle.has_overlap(vehicle.get_box2d(), laneline[1])
     print("has_overlap with laneline[1]: ", bool_has_overlap)
-    bool_has_overlap = vehicle.has_overlap(laneline[2])
+    bool_has_overlap = Vehicle.has_overlap(vehicle.get_box2d(), laneline[2])
     print("has_overlap with laneline[2]: ", bool_has_overlap)
 
     plt.fill(*rect[0], color='gray', alpha=0.5)
