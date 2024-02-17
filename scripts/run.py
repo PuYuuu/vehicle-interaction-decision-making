e29ddd3e36d8aa73c8e7126cd65bba028d814afe
PuYuuu <yuu.pu@foxmail.com>
2024-02-17 15:15:36 +0800

    add L1 and L2

--- scripts/run.py
+++ scripts/run.py
@@ -1,4 +1,5 @@
 import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
@@ -38,18 +39,40 @@ class KLevelPlanner:
         self.weight_avoid = 200
         self.weight_safe = 20
         self.weight_offroad = 100
-        self.weight_direction = 10
-        self.weight_distance = 1
+        self.weight_direction = 0
+        self.weight_distance = 10
 
 
     def planning(self, ego:Vehicle, other:Vehicle):
+        other_prediction = self.get_prediction(ego, other)
+        actions_id, _ =  self.helper(ego, other, other_prediction)
+
+        return KLevelPlanner.action_sets[actions_id[0]][0], KLevelPlanner.action_sets[actions_id[0]][1]
+
+
+    def update(self, node:Node, act):
+        acc = act[0]
+        omega = act[1]
+
+        x = node.x + node.v * np.cos(node.yaw) * self.dt
+        y = node.y + node.v * np.sin(node.yaw) * self.dt
+        v = node.v + acc * self.dt
+        yaw = node.yaw + omega * self.dt
+
+        while yaw > 2 * np.pi:
+            yaw -= 2 * np.pi
+        while yaw < 0:
+            yaw += 2 * np.pi
+
+        return x, y, yaw, v
+
+
+    def helper(self, ego:Vehicle, other:Vehicle, traj):
         root = Node(ego.x, ego.y, ego.yaw, ego.v)
         queue = deque()
         queue.append(root)
-
-        other_prediction = self.get_prediction(ego, other)
-
         step = 0
+
         while len(queue) > 0:
             queue_len = len(queue)
             
@@ -61,10 +84,10 @@ class KLevelPlanner:
                     ego_safezone = ego.get_safezone([x, y])
 
                     avoid = 0
-                    if Vehicle.has_overlap(ego_box2d, other.get_box2d(other_prediction[step])):
+                    if Vehicle.has_overlap(ego_box2d, other.get_box2d(traj[step])):
                         avoid = -1
                     safe = 0
-                    if Vehicle.has_overlap(ego_safezone, other.get_safezone(other_prediction[step])):
+                    if Vehicle.has_overlap(ego_safezone, other.get_safezone(traj[step])):
                         safe = -1
                     offroad = 0
                     for rect in self.env.rect:
@@ -86,30 +109,19 @@ class KLevelPlanner:
             step += 1
             if step >= self.steps:
                 break
+
         node = max(queue, key=lambda node:node.reward)
-        action_id = node.action
+        action_id_list = []
+        pos_list = []
         while node.parent != None:
-            action_id = node.action
+            action_id_list.append(node.action)
+            pos_list.append([node.x, node.y])
             node = node.parent
 
-        return KLevelPlanner.action_sets[action_id][0], KLevelPlanner.action_sets[action_id][1]
+        actions = action_id_list[::-1]
+        expected_traj = pos_list[::-1]
 
-
-    def update(self, node:Node, act):
-        acc = act[0]
-        omega = act[1]
-
-        x = node.x + node.v * np.cos(node.yaw) * self.dt
-        y = node.y + node.v * np.sin(node.yaw) * self.dt
-        v = node.v + acc * self.dt
-        yaw = node.yaw + omega * self.dt
-
-        while yaw > 2 * np.pi:
-            yaw -= 2 * np.pi
-        while yaw < 0:
-            yaw += 2 * np.pi
-
-        return x, y, yaw, v
+        return actions, expected_traj
 
 
     def get_prediction(self, ego: Vehicle, other: Vehicle):
@@ -117,9 +129,14 @@ class KLevelPlanner:
         if ego.level == 0:
             return [[other.x, other.y]] * self.steps
         elif ego.level == 1:
-            pass
+            other_prediction_ego = [[ego.x, ego.y]] * self.steps
+            other_act, other_traj = self.helper(other, ego, other_prediction_ego)
+            return other_traj
         elif ego.level == 2:
-            pass
+            static_traj = [[other.x, other.y]] * self.steps
+            _, ego_l0_traj = self.helper(ego, other, static_traj)
+            _, other_l1_traj = self.helper(other, ego, ego_l0_traj)
+            return other_l1_traj
 
 
     def is_opposite_direction(self, x, y, yaw, ego_box2d):
@@ -152,44 +169,58 @@ class KLevelPlanner:
 
 def run():
     env = EnvCrossroads(size=25, lanewidth=4)
-    vehicle_0 = Vehicle(2, -16, np.pi / 2, 'blue')  # the straight vehicle
-    vehicle_1 = Vehicle(-2, 16, -np.pi / 2, 'red')  # the turn left vehicle 
 
-    vehicle_0.set_level(0)
-    vehicle_1.set_level(0)
-    vehicle_0.set_target([2, 16])
-    vehicle_1.set_target([15, -2])
+    succeed_count = 0
+    for iter in range(100):
+        init_y_0 = random.uniform(-20, -12)
+        init_y_1 = random.uniform(12, 20)
+        init_v_0 = random.uniform(3, 5)
+        init_v_1 = random.uniform(3, 5)
 
-    planner = KLevelPlanner(env)
+        vehicle_0 = Vehicle(2, init_y_0, np.pi / 2, 'blue')   # the straight vehicle
+        vehicle_1 = Vehicle(-2, init_y_1, -np.pi / 2, 'red')  # the turn left vehicle 
 
-    while True:
-        if np.hypot(vehicle_1.target[0] - vehicle_1.x, vehicle_1.target[1] - vehicle_1.y) < 1 and \
-           np.hypot(vehicle_0.target[0] - vehicle_0.x, vehicle_0.target[1] - vehicle_0.y) < 1:
-            print("success !")
-            break
+        vehicle_0.set_level(0)
+        vehicle_1.set_level(1)
+        vehicle_0.set_target([2, 16])
+        vehicle_1.set_target([15, -2])
+        vehicle_0.v = init_v_0
+        vehicle_1.v = init_v_1
 
-        if Vehicle.has_overlap(vehicle_1.get_box2d(), vehicle_0.get_box2d()):
-            print("failed, exit...")
-            break
+        planner = KLevelPlanner(env)
 
-        acc, omega = planner.planning(vehicle_1, vehicle_0)
-        vehicle_1.update(acc, omega, DT)
+        cur_loop_count = 0
+        while True:
+            if np.hypot(vehicle_1.target[0] - vehicle_1.x, vehicle_1.target[1] - vehicle_1.y) < 1 and \
+            np.hypot(vehicle_0.target[0] - vehicle_0.x, vehicle_0.target[1] - vehicle_0.y) < 1:
+                print("success !")
+                succeed_count += 1
+                break
 
-        acc, omega = planner.planning(vehicle_0, vehicle_1)
-        vehicle_0.update(acc, omega, DT)
+            if Vehicle.has_overlap(vehicle_1.get_box2d(), vehicle_0.get_box2d()) or cur_loop_count > 36:
+                print("failed, exit...")
+                break
 
-        plt.cla()
-        env.draw_env()
-        vehicle_0.draw_vehicle()
-        vehicle_1.draw_vehicle()
-        plt.xlim(-25, 25)
-        plt.ylim(-25, 25)
-        plt.gca().set_aspect('equal')
-        plt.pause(0.1)
-    plt.xlim(-25, 25)
-    plt.ylim(-25, 25)
-    plt.gca().set_aspect('equal')
-    plt.show()
+            acc, omega = planner.planning(vehicle_1, vehicle_0)
+            vehicle_1.update(acc, omega, DT)
+
+            acc, omega = planner.planning(vehicle_0, vehicle_1)
+            vehicle_0.update(acc, omega, DT)
+
+            plt.cla()
+            env.draw_env()
+            vehicle_0.draw_vehicle()
+            vehicle_1.draw_vehicle()
+            plt.xlim(-25, 25)
+            plt.ylim(-25, 25)
+            plt.gca().set_aspect('equal')
+            plt.pause(0.1)
+            cur_loop_count += 1
+    print(succeed_count)
+    # plt.xlim(-25, 25)
+    # plt.ylim(-25, 25)
+    # plt.gca().set_aspect('equal')
+    # plt.show()
 
 
 if __name__ == "__main__":
